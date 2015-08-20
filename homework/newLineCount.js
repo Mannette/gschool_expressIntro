61ce32f84e90fa930b6c111012289688de866d64
ethan <ethanmannette@gmail.com>
2015-08-20 11:52:40 -0600

    Added new folder. Finished synchronous and asynchronous file reading.

---
+++ homework/newLineCount.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var file = 'test.txt';
+
+// synchronous reading
+var lines = fs.readFileSync(file).toString().split('\n').length - 2;
+
+console.log(lines);
+
+// asynchronous reading
+fs.readFile(file, function (err, contents) {
+
+  var lines = contents.toString().split('\n').length-2;
+
+  console.log(lines);
+
+});
